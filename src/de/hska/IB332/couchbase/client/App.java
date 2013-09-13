53b00fd67e7c6cb9898e80e36af858bd7cc44b2c
Avenel <martin.briewig@googlemail.com>
2013-09-13 12:19:11 +0200

    added getView. implemented simple example.

--- src/de/hska/IB332/couchbase/client/App.java
+++ src/de/hska/IB332/couchbase/client/App.java
@@ -1,5 +1,8 @@
 package de.hska.IB332.couchbase.client;
 
+import com.couchbase.client.protocol.views.ViewResponse;
+import com.couchbase.client.protocol.views.ViewRow;
+
 import de.hska.IB332.couchbase.service.CouchbaseService;
 import de.hska.IB332.couchbase.service.CouchbaseServiceFactory;
 
@@ -10,13 +13,30 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		try {
-			CouchbaseService service = CouchbaseServiceFactory.getService("localhost", "Administrator", "");
+			CouchbaseService service = CouchbaseServiceFactory.getService("localhost", "", "");
 			
-			service.createView("dev_beer", "by_name1");
+			// create simple view
+			String mapFunction =
+		            "function (doc, meta) {\n" +
+		            "  if(doc.type && doc.type == \"beer\") {\n" +
+		            "    emit(doc.name);\n" +
+		            "  }\n" +
+		            "}";
+			
+			String reduceFunction = "_count";
+			
+			service.createView("beerTwo", "by_name", mapFunction, reduceFunction);
+			
+			// get simple view
+			ViewResponse response = service.getView("beerTwo", "by_name", 10);
+			
+			// Print value (5891 = beer count)
+			for(ViewRow row : response) {
+				System.out.println("Beer count: " + row.getValue());
+			}
 			
 			service.closeConnection();
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
 	}
--- src/de/hska/IB332/couchbase/service/CouchbaseService.java
+++ src/de/hska/IB332/couchbase/service/CouchbaseService.java
@@ -7,13 +7,24 @@ import com.couchbase.client.CouchbaseClient;
 import com.couchbase.client.CouchbaseConnectionFactory;
 import com.couchbase.client.CouchbaseConnectionFactoryBuilder;
 import com.couchbase.client.protocol.views.DesignDocument;
+import com.couchbase.client.protocol.views.Query;
+import com.couchbase.client.protocol.views.Stale;
+import com.couchbase.client.protocol.views.View;
 import com.couchbase.client.protocol.views.ViewDesign;
+import com.couchbase.client.protocol.views.ViewResponse;
 
 public class CouchbaseService {
 	
 	private String url, user, password;
 	private CouchbaseClient client;
 	
+	/**
+	 * public constructor. Connects to CouchBase Server.
+	 * @param url
+	 * @param user
+	 * @param password
+	 * @throws Exception
+	 */
 	public CouchbaseService(String url, String user, String password) throws Exception {
 		this.url = url;
 		this.user = user;
@@ -21,27 +32,25 @@ public class CouchbaseService {
 		connect();
 	}
 	
+	/**
+	 * Connects to CouchBase Server
+	 * @throws Exception
+	 */
 	public void connect() throws Exception {
 		System.setProperty("viewmode", "development");
 		ArrayList<URI> nodes = new ArrayList<URI>();
 
 	    // Add one or more nodes of your cluster (exchange the IP with yours)
 	    nodes.add(URI.create("http://"+this.url+":8091/pools"));
-
-	    CouchbaseConnectionFactoryBuilder m_couchbaseConnectionFactoryBuilder = new CouchbaseConnectionFactoryBuilder();
-	    m_couchbaseConnectionFactoryBuilder.setMaxReconnectDelay(10000);
-	    m_couchbaseConnectionFactoryBuilder.setOpQueueMaxBlockTime(100);
-	    m_couchbaseConnectionFactoryBuilder.setOpTimeout(20000);
-	    m_couchbaseConnectionFactoryBuilder.setShouldOptimize(true);
-	    m_couchbaseConnectionFactoryBuilder.setViewTimeout(300000);
-
-	    CouchbaseConnectionFactory connFactory = m_couchbaseConnectionFactoryBuilder.buildCouchbaseConnection(nodes, "beer-sample", "");
 	    
 	    // Try to connect to the client
 	    this.client = null;
-	    client = new CouchbaseClient(connFactory);//this.user, this.password);
+	    client = new CouchbaseClient(nodes, "beer-sample", this.user, this.password);
 	}
 	
+	/**
+	 * Close connection.
+	 */
 	public void closeConnection() {
 		// Shutdown the client
 	    this.client.shutdown();
@@ -49,25 +58,35 @@ public class CouchbaseService {
 	
 	
 	/**
-	 * This function builds a map/reduce view
+	 * Builds a map/reduce view.
 	 * @param bucket
 	 * @param viewName
 	 */
-	public void createView(String bucket, String viewName) {
-		DesignDocument designDoc = new DesignDocument(bucket);
-		
-		String mapFunction = 
-				"function (doc, meta) {\n" +
-				"  if (doc.type && doc.type == 'beer') {\n" +
-				"    emit(doc.name);\n" +
-				"  }\n" +
-				"}";
-		
-		String reduceFunction = "";
+	public void createView(String ddName, String viewName, String mapFunction, String reduceFunction) {
+		DesignDocument designDoc = new DesignDocument(ddName);		
 		
 		ViewDesign viewDesign = new ViewDesign(viewName, mapFunction, reduceFunction);
 		designDoc.getViews().add(viewDesign);
+		
 		this.client.createDesignDoc(designDoc);
 	}
+	
+	/**
+	 * Returns the result of a MapReduce action of a given view.
+	 * @param ddName
+	 * @param viewName
+	 * @param limit
+	 * @return ViewResponse result
+	 */
+	public ViewResponse getView(String ddName, String viewName, int limit) {
+		View view = client.getView(ddName, viewName);
+		Query query = new Query();
+		query.setIncludeDocs(true).setLimit(limit);
+		query.setStale( Stale.FALSE );
+		
+		ViewResponse result = client.query(view, query);
+		return result;
+	}
+	
 	
 }
